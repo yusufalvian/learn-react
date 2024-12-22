512b789b16a2f201c1febcfc78c1a8b70a4ab69e
wanda <wanda.alfian@mceasy.com>
2024-12-22 13:28:43 +0700

    learn

--- 02. Data-Driven React/01. Section 2 Intro/App.jsx
+++ 02. Data-Driven React/01. Section 2 Intro/App.jsx
@@ -2,6 +2,22 @@ import Header from "./components/Header"
 import Entry from "./components/Entry"
 import data from "./data"
 
+/**
+<Entry
+    key={1}
+    id={1}
+    img={{
+        src: "https://scrimba.com/links/travel-journal-japan-image-url",
+        alt: "Mount Fuji"
+    }}
+    title="Mount Fuji"
+    country="Japan"
+    googleMapsLink="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
+    dates="12 Jan, 2021 - 24 Jan, 2021"
+    text="Mount Fuji is the tallest mountain..."
+/>
+ */
+
 export default function App() {
     
     const entryElements = data.map((entry) => {
